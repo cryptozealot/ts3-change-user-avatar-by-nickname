a57ecd79bab36dcbe9770a5e3220d05371f4bad7
Crypto Zealot <34049724+cryptozealot@users.noreply.github.com>
2017-12-05 18:14:01 +0200

    Update ts3chavatar.sh

--- ts3chavatar.sh
+++ ts3chavatar.sh
@@ -4,7 +4,7 @@ nickname=$1
 picpath=$2
 pathtots=$3
 
-#Get UID and ID
+#Get UID and ID from database by nickname
 uid=`sqlite3 -batch /home/teamspeak/ts3server.sqlitedb "SELECT client_unique_id from clients where client_nickname='$nickname';"`
 id=`sqlite3 -batch /home/teamspeak/ts3server.sqlitedb "SELECT client_id from clients where client_nickname='$nickname';"`
 
@@ -14,7 +14,6 @@ hexuid=`xxd -p <<< $b64decodeduid`
 hexuid=${hexuid:0:40}
 result=`echo $hexuid | tr 'a' 'k' | tr 'b' 'l' | tr 'c' 'm' | tr 'd' 'n' | tr 'e' 'o' | tr 'f' 'p'`
 result=`echo $result | tr '0' 'a' | tr '1' 'b' | tr '2' 'c' | tr '3' 'd' | tr '4' 'e' | tr '5' 'f' | tr '6' 'g' | tr '7' 'h' | tr '8' 'i' | tr '9' 'j'`
-
 avatarfilename="avatar_$result"
 echo avatar is $avatarfilename
 
