7acb26e7217ab722f5debd19d99e0f9c125ab1fb
Crypto Zealot <34049724+cryptozealot@users.noreply.github.com>
2017-12-05 18:11:43 +0200

    Update ts3chavatar.sh

--- ts3chavatar.sh
+++ ts3chavatar.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
+# Input parameters
+nickname=$1
+picpath=$2
+pathtots=$3
 
-# Input parameter - UID.
-uid=$1
+#Get UID and ID
+uid=`sqlite3 -batch /home/teamspeak/ts3server.sqlitedb "SELECT client_unique_id from clients where client_nickname='$nickname';"`
+id=`sqlite3 -batch /home/teamspeak/ts3server.sqlitedb "SELECT client_id from clients where client_nickname='$nickname';"`
 
-#Decode Base64 UID to HEX.
+#Generate avatar filename - base64 decode, hexdump, replace letter by letter
 b64decodeduid=`base64 -d <<< $uid`
-#Hexdump to ASCI string.
 hexuid=`xxd -p <<< $b64decodeduid`
-
-#Cut last two characters, for some reason there are 2 more characters than we need.
 hexuid=${hexuid:0:40}
-
-#Replace letters in string (probabbly there's a better and faster way, but it works :))
 result=`echo $hexuid | tr 'a' 'k' | tr 'b' 'l' | tr 'c' 'm' | tr 'd' 'n' | tr 'e' 'o' | tr 'f' 'p'`
+result=`echo $result | tr '0' 'a' | tr '1' 'b' | tr '2' 'c' | tr '3' 'd' | tr '4' 'e' | tr '5' 'f' | tr '6' 'g' | tr '7' 'h' | tr '8' 'i' | tr '9' 'j'`
 
-#Replace digits in string.
-result=`echo $result | tr '0' 'a' | tr '1' 'b' | tr '2' 'c' | tr '3' 'd' | tr '4' 'e'  | tr '5' 'f' | tr '6' 'g' | tr '7' 'h' | tr '8' 'i' | tr '9' 'j'`
+avatarfilename="avatar_$result"
+echo avatar is $avatarfilename
 
-result="avatar_$result"
+#Get MD5 of new picture
+md5hash=`md5sum  $picpath | awk '{print $1}'`
+echo md5 is $md5hash
 
-echo $result
+#Replace old pic file with new pic
+cp $picpath $pathtots/files/virtualserver_1/internal/$avatarfilename
+echo path is $pathtots/files/virtualserver_1/internal/$avatarfilename
+
+#Update new md5hash of new pic in database
+sqlite3 -batch /home/teamspeak/ts3server.sqlitedb "UPDATE client_properties SET value='$md5hash' WHERE ident='client_flag_avatar' AND id='$id';"
